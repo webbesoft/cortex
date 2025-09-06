6917e4908fdac8823de4092b0d489e4fc8b05fb8
Tawanda <ejmunongo@gmail.com>
2025-09-06 12:28:18 +0200

    add strict mode and force https in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+        Model::shouldBeStrict(! app()->isProduction());
     }
 }
