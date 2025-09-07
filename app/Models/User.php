18c4230cf496119788b7587e0aba57fd00bf5edd
Tawanda <ejmunongo@gmail.com>
2025-09-07 10:46:00 +0200

    fix filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -68,5 +71,11 @@ class User extends Authenticatable
     public function xp_events(): HasMany
     {
         return $this->hasMany(XpEvent::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+       return str_ends_with($this->email, '@webbe.dev');
+    //    && $this->hasVerifiedEmail();
     }
 }
